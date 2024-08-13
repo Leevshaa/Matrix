62aaa13b5a969619e9e6a60a086f39b66631adf9
Vitalii <vitaliti.yurchenko@gmail.com>
2024-08-14 01:39:26 +0300

    Add check magic square

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,13 +2,15 @@ package app;
 
 public class Main {
 
+    final static int magic = 4;
+
     public static void main(String[] args) {
 
         int [][] matrix = {
-                {2, 8, 4, 7},
-                {3, 1, 5, 11},
-                {9, 10, 12, 13},
-                {17, 7, 10, 8}
+                {5, 28, 13, 7},
+                {9, 33, 16, 1},
+                {2, 28, 34, 8},
+                {5, 12, 20, 4}
         };
 
         System.out.println("Матриця 4x4: ");
@@ -60,6 +62,37 @@ public class Main {
                 ") : " + multiEvenColumns + "\nДобуток елементів у непарних стовпцях (стовпець " + firstOddColumns +
                 ", " + secondOddColumns + ") : " + multiOddColumns);
 
-        //TODO Магічний квадрат
+        // За замовчуванням вважаємо, що матриця є магічним квадратом
+        boolean isMagicSquare = true;
+
+        int sumMainDiagonal = 0; //<-змінна, яка буде накопичувати суму чисел головної діагоналі
+        int sumSideDiagonal = 0; //<-змінна, яка буде накопичувати суму чисел побічної діагоналі
+
+        for (int i = 0; i < magic; i++){
+            //[i][i] оскільки числа головної діагоналі матриці будуть мати індекси [0][0], [1][1] і т.д.
+            sumMainDiagonal += matrix[i][i];
+            //Побічна діагональ [0] та [4 - 1 - 0], [1][4 - 1 - 1]...
+            sumSideDiagonal += matrix[i][magic - 1 - i];
+        }
+        if (sumMainDiagonal != sumSideDiagonal)
+            isMagicSquare = false; // Визначаємо чи рівна сума двох діагоналей
+
+        for (int i = 0; i < magic; i++){
+            int totalRows = 0; //<-змінна, яка буде накопичувати суму чисел у рядках
+            int totalColumns = 0; //<-змінна, яка буде накопичувати суму чисел у стовбцях
+
+            for (int j = 0; j < magic; j++){
+                totalRows += matrix[i][j]; // Проходимо по рядках масиву
+                totalColumns += matrix[j][i]; // Проходимо по стовбцях масиву
+            }
+            //Якщо сума рядків, сума стовбців та сума чисел по діагоналі не рівні між собою,
+            //то матриця не є магічним квадратом
+            if (totalRows != totalColumns || totalColumns != sumMainDiagonal)
+                isMagicSquare = false;
+        }
+        if (isMagicSquare)
+            System.out.println("\nВідчувається якась магія, оскільки ваша матриця є магічним квадратом! :)");
+        else
+            System.out.println("\nМатриця не є магічним квадратом.");
     }
 }
